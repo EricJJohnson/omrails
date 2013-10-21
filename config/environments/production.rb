e058a054d9fa775754c1208d0537ff497ec3512a
eric <eric@ericslab.com>
2013-10-20 22:12:29 -0700

    connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,18 @@ Omrails::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  #Configuring Amazon S3 for Paperclip file uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+	}
+
+end
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
